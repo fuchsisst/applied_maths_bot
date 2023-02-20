0b8e42f801749786c66a4c6c416c232f78514599
Borschik0 <vitalinastepnikova@gmail.com>
2023-02-20 20:37:10 +0200

    Commit 1: Add some commands

--- bin/applied_maths_bot.dart
+++ bin/applied_maths_bot.dart
@@ -1,165 +1,68 @@
 import 'dart:async';
 
+import 'package:applied_maths_bot/command_text.dart';
 import 'package:teledart/model.dart';
 import 'package:teledart/teledart.dart';
 import 'package:teledart/telegram.dart';
-import 'dart:io' as io;
 
-const telegramApiKey = '5839246590:AAGamMdzCPzjFn0xgKCl0nmk7juW8oAG4eI';
+var BOT_TOKEN = '5839246590:AAGamMdzCPzjFn0xgKCl0nmk7juW8oAG4eI';
 
 Future<void> main() async {
-
   // init Telegram
-  final telegram = Telegram(telegramApiKey);
-  final event = Event((await telegram.getMe()).username!);
-  final teledart = TeleDart(telegramApiKey, event);
+  final username = (await Telegram(BOT_TOKEN).getMe()).username;
+  var teledart = TeleDart(BOT_TOKEN, Event(username!));
 
-  // start bot
   teledart.start();
 
-  // when user input command with keyword 'start', execute this block
-  teledart.onCommand('start').listen((message) {
+  //KeyBoardButton for main menu
+  final info113 = KeyboardButton(text: 'Інфо');
+  final socialMedia = KeyboardButton(text: 'Соцмережі');
+  final news = KeyboardButton(text: 'Новини');
+  final fAQ = KeyboardButton(text: 'FAQ');
 
-    // create buttons
-    final testButton = KeyboardButton(text: 'Test medium');
-    final anotheButton = KeyboardButton(text: 'Another button');
-    final loadImages = KeyboardButton(text: 'Load images');
-    final deleteImages = KeyboardButton(text: 'Delete images');
+  List<List<KeyboardButton>> keyboardMenu = [
+    [info113],
+    [socialMedia],
+    [news],
+    [fAQ]
+  ];
 
-    // arrange buttons boy two in line
-    final twoButtonList = [testButton, anotheButton];
-    final imageButtonList = [loadImages, deleteImages];
+  final markupMenu =
+      ReplyKeyboardMarkup(resizeKeyboard: true, keyboard: keyboardMenu);
 
-    // grab all buttons together
-    final markup = ReplyKeyboardMarkup(
-      keyboard: [twoButtonList, imageButtonList],
-    );
+  //
+  final instagram = InlineKeyboardButton(
+      text: 'Instagram',
+      url:
+          'https://www.instagram.com/appliedmathematics_onpu/?igshid=YmMyMTA2M2Y%3D');
+  final facebook = InlineKeyboardButton(
+      text: 'Facebook', url: 'https://m.facebook.com/AppliedMathematicsONPU/');
+  final telegramChannel = InlineKeyboardButton(
+      text: 'Telegram Channel', url: 'https://t.me/applyed_math');
 
-    // send message to user
-    teledart.sendMessage(
-      message.chat.id,
-      'Try it',
-    );
-  });
+  List<List<InlineKeyboardButton>> keyboardSocialMedia = [
+    [instagram],
+    [facebook],
+    [telegramChannel]
+  ];
 
-  // when user input text with keyword 'Another button', execute this block
-  teledart.onMessage(keyword: 'Another button').listen((message) {
-    final testButton = KeyboardButton(text: 'Test medium');
-    final anotheButton = KeyboardButton(text: 'Another button');
-    final loadImages = KeyboardButton(text: 'Load images');
-    final deleteImages = KeyboardButton(text: 'Delete images');
+  final markupSocialMedia =
+      InlineKeyboardMarkup(inlineKeyboard: keyboardSocialMedia);
 
-    final twoButtonList = [testButton, anotheButton];
-    final imageButtonList = [loadImages, deleteImages];
+  teledart.onCommand('start').listen(
+      (message) => message.reply(startMessage, replyMarkup: markupMenu));
+  teledart
+      .onCommand('info_113')
+      .listen((message) => message.reply(info_113, replyMarkup: markupMenu));
 
-    final markup = ReplyKeyboardMarkup(
-      keyboard: [twoButtonList, imageButtonList],
-    );
+  teledart
+      .onMessage(keyword: info113.text)
+      .where((message) => message.text?.contains(info113.text) ?? false)
+      .listen((message) => message.reply(info_113));
 
-    teledart.sendMessage(
-      message.chat.id,
-      'Try it: Another button',
-    );
-  });
-
-  // when user input text with keyword 'Test medium', execute this block
-  teledart.onMessage(keyword: 'Test medium').listen((message) {
-    final testButton = KeyboardButton(text: 'Test medium');
-    final anotheButton = KeyboardButton(text: 'Another button');
-    final loadImages = KeyboardButton(text: 'Load images');
-    final deleteImages = KeyboardButton(text: 'Delete images');
-
-    final twoButtonList = [testButton, anotheButton];
-    final imageButtonList = [loadImages, deleteImages];
-
-    final markup = ReplyKeyboardMarkup(
-      keyboard: [twoButtonList, imageButtonList],
-    );
-
-    teledart.sendMessage(
-      message.chat.id,
-      'Try it: Test medium',
-    );
-  });
-
-  // if user typed and send to bot anything except those one in if-statement,
-  // execute block
-  teledart.onMessage().listen((message) {
-    if (message.text != 'Test medium' &&
-        message.text != 'Another button' &&
-        message.text != 'Load images' &&
-        message.text != 'Delete images' &&
-        message.photo == null) {
-      teledart.sendMessage(
-        message.chat.id,
-        'Nice try',
-      );
-    }
-  });
-
-  // execute this if user hit 'Load images' button
-  teledart.onMessage(keyword: 'Load images').listen((message) async {
-
-    // ask user to select one photo
-    teledart.sendMessage(message.chat.id, 'choose one');
-
-    // subscribe to user input
-    final subscription = teledart.onMessage().listen((_) {});
-
-    // listen subscription
-    subscription.onData((data) async {
-
-      // if user upload photo do the following
-      if (data.photo != null) {
-
-        // create the dicrectory on the host
-        final dir =
-        await io.Directory('/var/upload/${message.chat.id}').create();
-
-        // download photo to the directory
-        downloadPhoto(
-          '${dir.path}/img.png',
-          telegramApiKey,
-          data,
-          teledart,
-        ).then((value) =>
-            teledart.sendMessage(data.chat.id, 'image loaded'));
-      }
-    });
-  });
-
-  // execute this if user hit 'Delete images' button
-  teledart.onMessage(keyword: 'Delete images').listen((message) {
-    // delete folder and image in it
-    deletePhotosDirectory(teledart, message).then((value) =>
-        teledart.sendMessage(message.chat.id, 'image deleted'));
-  });
-}
-
-Future<void> downloadPhoto(
-    String path,
-    String telegramApiKey,
-    TeleDartMessage message,
-    TeleDart teledart,
-    ) async {
-  final tPhoto = message.photo!.last;
-  final tFile = await teledart.getFile(tPhoto.fileId);
-  final tFileLink = tFile.getDownloadLink(telegramApiKey);
-  final request = await io.HttpClient().getUrl(Uri.parse(tFileLink!));
-  final response = await request.close();
-  await response.pipe(io.File(path).openWrite()) as io.File;
-}
-
-Future<void> deletePhotosDirectory(
-    TeleDart teledart,
-    TeleDartMessage message,
-    ) async {
-  final dir = io.Directory('/var/upload/${message.chat.id}');
-  final isDirExist = await dir.exists();
-  if (isDirExist) {
-    await io.Directory('/var/upload/${message.chat.id}')
-        .delete(recursive: true);
-  } else {
-    teledart.sendMessage(message.chat.id, 'Nothing to delete');
-  }
+  teledart
+      .onMessage(keyword: socialMedia.text)
+      .where((message) => message.text?.contains(socialMedia.text) ?? false)
+      .listen((message) =>
+          message.reply('Social Media:', replyMarkup: markupSocialMedia));
 }
