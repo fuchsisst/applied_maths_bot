83183d4fe27b3c7d9af71764f2307992736b1175
SVV <SVV@d.c>
2023-03-28 21:33:34 +0300

    Check

--- bin/applied_maths_bot.dart
+++ bin/applied_maths_bot.dart
@@ -65,8 +65,8 @@ Future<void> main() async {
   teleDart
       .onMessage(keyword: back.text)
       .where((message) => message.text?.contains(back.text) ?? false)
-      .listen((message) => message.reply('',
-    allowSendingWithoutReply: true,
+      .listen((message) => message.reply(
+    'Головне меню',
     replyMarkup: markupMenu,
   ));
 
