f436cada2e673dd3e747dd10b5394509bd029904
-Thanatos- <nikazavgorodniaya@gmail.com>
2023-04-10 20:44:15 +0300

    added a description to the a About button

--- lib/keyboards/main_menu_keyboard/info_keyboard/applied_math_113_keyboard/applied_math_113_function.dart
+++ lib/keyboards/main_menu_keyboard/info_keyboard/applied_math_113_keyboard/applied_math_113_function.dart
@@ -1,6 +1,5 @@
 import 'package:applied_maths_bot/command_text.dart';
 import 'package:teledart/teledart.dart';
-import '../info_keyboard.dart';
 import 'applied_math_113_keyboard.dart';
 
 void appliedMathFunc(TeleDart teleDart){
@@ -8,6 +7,6 @@ void appliedMathFunc(TeleDart teleDart){
       .onMessage(keyword: about113.text)
       .where((message) => message.text?.contains(about113.text) ?? false)
       .listen((message) => message.reply(
-    info_113,
+    aboutAppliedMath,
   ));
 }
