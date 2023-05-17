55578cdc9e1f9ed65c7597f9b1dc183afedd4203
SVV <vitalinastepnikova@gmail.com>
2023-05-18 00:21:05 +0300

    Test Chat #2

--- lib/keyboards/main_menu_keyboard/faqs_keyboard/chat.dart
+++ lib/keyboards/main_menu_keyboard/faqs_keyboard/chat.dart
@@ -39,6 +39,9 @@ void chattingFunc(TeleDart teleDart, Telegram bot) {
       // userStates.remove(chatUserId);
 
       message.reply('Сообщение отправлено админу. Спасибо!');
+    } else if (adminStates.containsKey(chatAdminId) && adminStates[chatAdminId] == 'chatting') {
+      // Отправляем сообщение клиенту
+      messageToClient(adminStates, teleDart, message, bot, userStates);
     }
   });
 
@@ -49,19 +52,11 @@ Future<void> messageToAdmin ( Map<int, String> userStates, Map<int, String> admi
   final chatId = message.chat.id;
   final text = message.text;
 
-  if(adminStates.containsKey(chatId) && adminStates[chatId] == 'chatting'){
+  if (adminStates.containsKey(chatId) && adminStates[chatId] == 'chatting') {
     await bot.sendMessage(adminChatId, "Cообщение от клиента $chatUser: $text");
-    teleDart.onMessage().listen((message) async {
-      final chatId = message.chat.id;
-      print(chatId);
-      if (userStates.containsKey(chatId) && userStates[chatId] == 'chatting') {
-        // Отправляем сообщение админу
-        messageToAdmin(userStates, adminStates, teleDart, message, bot);
-
-      }
-    });
+  } else {
+    await bot.sendMessage(adminChatId, "Новое сообщение от клиента $chatUser: $text");
   }
-  await bot.sendMessage(adminChatId, "Новое сообщение от клиента $chatUser: $text");
 
   teleDart.onMessage(keyword: 'Відповісти').listen((message) async {
     final chatId = message.chat.id;
