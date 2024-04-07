8e95ac00c48a86293523ac0536685e3d95f6d504
Vitalina Stepnikova <72044379+fuchsisst@users.noreply.github.com>
2024-04-07 15:21:49 +0300

    Update system_analisys_124_keyboard.dart
    Changed 2023 year to 2024

--- lib/keyboards/main_menu_keyboard/info_keyboard/system_analysis_124_keyboard/system_analisys_124_keyboard.dart
+++ lib/keyboards/main_menu_keyboard/info_keyboard/system_analysis_124_keyboard/system_analisys_124_keyboard.dart
@@ -3,7 +3,7 @@ import 'package:teledart/model.dart';
 //KeyBoardButton for 124 - System Analysis
 final about124 = KeyboardButton(text: 'Про спеціальність 124');
 final statistics = KeyboardButton(text: 'Статистика ринку для 124');
-final admissionTips = KeyboardButton(text: 'Вступ 2023 для 124');
+final admissionTips = KeyboardButton(text: 'Вступ 2024 для 124');
 final backInfo = KeyboardButton(text: 'Back');
 
 List<List<KeyboardButton>> keyboardMenu = [
