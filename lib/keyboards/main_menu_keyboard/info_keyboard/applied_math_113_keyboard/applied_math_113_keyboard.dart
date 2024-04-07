17a1f755f2effc5c9b225471e786486e1abdf11f
Vitalina Stepnikova <72044379+fuchsisst@users.noreply.github.com>
2024-04-07 15:20:31 +0300

    Update applied_math_113_keyboard.dart
    Changed 2023 year to 2024

--- lib/keyboards/main_menu_keyboard/info_keyboard/applied_math_113_keyboard/applied_math_113_keyboard.dart
+++ lib/keyboards/main_menu_keyboard/info_keyboard/applied_math_113_keyboard/applied_math_113_keyboard.dart
@@ -3,7 +3,7 @@ import 'package:teledart/model.dart';
 //KeyBoardButton for 113 - Applied Math
 final about113 = KeyboardButton(text: 'Про спеціальність 113');
 final statistics = KeyboardButton(text: 'Статистика ринку для 113');
-final admissionTips = KeyboardButton(text: 'Вступ 2023 для 113');
+final admissionTips = KeyboardButton(text: 'Вступ 2024 для 113');
 final backInfo = KeyboardButton(text: 'Back');
 
 List<List<KeyboardButton>> keyboardMenu = [
